2ec5569d103ef0ac1a0da6b8036c383be1a9842f
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-09-17 17:30:24 +0300

    hgbfrfvdvfgbrtfd3444 23 43234

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.properties import NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
@@ -6,6 +7,7 @@ from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.animation import Animation
 
 Window.size = (800, 600)
 
@@ -34,28 +36,58 @@ class  GameSelectionScreen(Screen):
         self.manager.current = 'football'
 
     def __init__(self, **kwargs):
-        super( GameSelectionScreen, self).__init__(**kwargs)
+        super(GameSelectionScreen, self).__init__(**kwargs)
         layout = BoxLayout(orientation='vertical')
         self.add_widget(layout)
 
-        play_button = Button(text='баксетбол', size_hint=(1, 0.2))
-        play_button.bind(on_press=self.go_to_game_basketbal)
-        layout.add_widget(play_button)
+        basketball = Button(text='баксетбол', size_hint=(1, 0.2))
+        basketball.bind(on_press=self.go_to_game_basketbal)
+        layout.add_widget(basketball)
 
-        play_button = Button(text='футбол', size_hint=(1, 0.2))
-        play_button.bind(on_press=self.go_to_game_football)
-        layout.add_widget(play_button)
+        football = Button(text='футбол', size_hint=(1, 0.2))
+        football.bind(on_press=self.go_to_game_football)
+        layout.add_widget(football)
+
+
+
+
 
 class Football(Screen):
+    score = NumericProperty(0)
     def __init__(self, **kwargs):
         super(Football).__init__(**kwargs)
         layout = FloatLayout()
         self.add_widget(layout)
 
-        self.background = Image(socure = 'football.png', size_hint=(1,1))
+        self.background = Image(socure='football.png', allo_stretch=True, keep_ration=False,  size_hint=(1,1))
         layout.add_widget(self.background)
 
-        self.ball = Image(sourse = "football ball.png",)
+        self.ball = Image(sourse = "football ball.png", allow_stretch=True , size=(100,100))
+        self.ball.pos_hint= {'center_x': 0.5, 'center_y': 0.5}
+        self.ball.bind(on_touch_down= self.ball_cliked)
+        layout.add_widget(self.ball)
+
+        self.score_label = Label(text='Score: 0',font_size='24sp', size_hint=(1, 0.1))
+        layout.add_widget(self.score_label)
+
+        self.back_button = Button(tet='Назад',size_hint=(1, 0.2))
+        self.back_button.bind(on_press=self.go_back)
+        layout.add_widget(self.back_button)
+
+    def ball_cliked(self, instance,touch):
+        if instance.collide_point(*touch.pos):
+            self.score += 1
+            self.score_label.text = f'Score: {self.score}'
+            self.ball_cliick_animation()
+
+    def ball_cliick_animation(self):
+        anim=Animation(size=(150,150)) + Animation(size=(100,100))
+        anim.start(self.ball)
+
+
+    def go_back(self, *args):
+        self.manager.current = 'game_selection'
+
 
 
 class ClikerApp(App):
@@ -63,6 +95,8 @@ class ClikerApp(App):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
         sm.add_widget(GameSelectionScreen(name='game_selection'))
+        sm.add_widget(Football(name='football'))
+        
         return sm
 
 if __name__ == '__main__':
